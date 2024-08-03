63a3fc906fb05b1b5c06e553d97639d539294030
Ian Wilson <ianwilson1337@gmail.com>
2024-08-03 17:10:08 -0500

    Adding missing test dependencies

--- jest.config.integration.js
+++ jest.config.integration.js
@@ -2,6 +2,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/e2e/**/*.ts?(x)', '**/?(*.)+(e2e).ts?(x)'],
-    testPathIgnorePatterns: ['/node_modules/']
+    testPathIgnorePatterns: ['/node_modules/'],
+    setupFiles: ['./jest.setup.js']
   };
   
--- jest.config.unit.js
+++ jest.config.unit.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
-  testPathIgnorePatterns: ['/node_modules/', '/e2e/']
+  testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
+  setupFiles: ['./jest.setup.js']
 };
