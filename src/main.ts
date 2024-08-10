27f6e8566a88f0f61c0e057baf0a64d04a7bb5ed
Ian Wilson <ianwilson1337@gmail.com>
2024-08-10 18:29:50 -0500

    Added Security to REST endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
 import { config } from "dotenv";
 import express, { Express } from "express";
-import {
-  InteractionType,
-  InteractionResponseType,
-  verifyKeyMiddleware,
-} from "discord-interactions";
 import { initializeBot } from "./bot";
 import { OpenAIClient } from "./adapters/openAI";
+import { authenticateToken, login } from "./middleware/authentication";
 
 config();
 
 export const main = (): Express => {
   // Create an express app
   const app = express();
+  // Middleware to parse JSON bodies
+  app.use(express.json());
+
+  // Middleware to parse URL-encoded bodies
+  app.use(express.urlencoded({ extended: true }));
   const publicKey = process.env.DISCORD_PUBLIC_KEY;
   if (!publicKey) {
     console.error("DISCORD_PUBLIC_KEY is not defined");
@@ -25,6 +26,11 @@ export const main = (): Express => {
   app.get("/healthCheck", (req, res) => {
     res.status(200).send(true);
   });
+
+  // Must login to use any other methods
+  app.post("/login", login);
+
+  app.use(authenticateToken);
 
   app.get("/assistants", async (req, res) => {
     const ai = OpenAIClient.Instance;
@@ -45,52 +51,6 @@ export const main = (): Express => {
     const runs = await ai.listRuns(threadId);
     res.status(200).send(runs);
   });
-
-  /**
-   * Interactions endpoint URL where Discord will send HTTP requests
-   * Parse request body and verifies incoming requests using discord-interactions package
-   */
-  app.post(
-    "/interactions",
-    verifyKeyMiddleware(publicKey),
-    async function (req, res) {
-      // Interaction type and data
-      const { type, data } = req.body;
-
-      /**
-       * Handle verification requests
-       */
-      if (type === InteractionType.PING) {
-        return res.send({ type: InteractionResponseType.PONG });
-      }
-
-      /**
-       * Handle slash command requests
-       * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
-       */
-      if (type === InteractionType.APPLICATION_COMMAND) {
-        const { name } = data;
-
-        // "test" command
-        if (name === "test") {
-          // Send a message into the channel where command was triggered from
-          return res.send({
-            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            data: {
-              // Fetches a random emoji to send from a helper function
-              content: `hello world`,
-            },
-          });
-        }
-
-        console.error(`unknown command: ${name}`);
-        return res.status(400).json({ error: "unknown command" });
-      }
-
-      console.error("unknown interaction type", type);
-      return res.status(400).json({ error: "unknown interaction type" });
-    }
-  );
 
   return app;
 };
