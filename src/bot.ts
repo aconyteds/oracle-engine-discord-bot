41e67cd86a205bbbf241dce1210f61c6e23399d8
Ian Wilson <ianwilson1337@gmail.com>
2024-08-10 23:30:07 -0500

    Removed unused im

--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,9 @@
 import { config } from "dotenv";
 import { DiscordClient } from "./adapters/discordClient";
-import { OpenAIClient } from "./adapters/openAI";
-import { DBClient } from "./adapters/MongoDB";
 
 config();
 
 export const initializeBot = async () => {
   const discordClient = DiscordClient.Instance;
-  await DBClient.Instance;
   await discordClient.initializeGateway();
 };
